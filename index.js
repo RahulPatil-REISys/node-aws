3ff88dbc6f55620cc71ed25aecd094a0a3de4727
Rahul <rahul.patil@reisystems.in>
2021-08-12 02:09:43 -0400

    misc

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const AWS = require('aws-sdk');
 const https = require('https');
+const fs = require('fs');
+
+
+AWS.config.update({
+    accessKeyId: 'AKIA6RC55GFFRMETMH7G',
+    secretAccessKey: '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',
+    region: 'us-east-1',
+  });
 
 var quicksight = new AWS.Service({
     apiConfig: require('./quicksight-2018-04-01.min.json'),
@@ -33,8 +41,13 @@ var quicksight = new AWS.Service({
 
 
 var express = require('express');
+var cors = require('cors')
+var options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/qa1.reisystems.in/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/qa1.reisystems.in/fullchain.pem'),
+};
 var app = express();
-
+app.use(cors());
 app.get('/url/:id', function(req, res){
     console.log(req.params.id)
     var dashboardId = '';
@@ -47,8 +60,25 @@ app.get('/url/:id', function(req, res){
     if(req.params.id == "RATING"){
         dashboardId = 'f0e81466-7d03-427b-8122-dcae0fcc624a';
     }
+    if(req.params.id == "VENDOR"){
+        dashboardId = '1b8f3b81-8145-4a47-9293-fcf3393e0bda';
+    }
+    if(req.params.id == "TOPVENDOR"){
+        dashboardId = '6f3650bf-600d-45a8-b4a6-726305e3cd72';
+    }
+    if(req.params.id == "OPPVEHICLE"){
+        dashboardId = '800ec831-da52-46f5-8888-f5668ea49b7d';
+    }
     
-    quicksight.getDashboardEmbedUrl({
+   if(req.params.id == "CONTRACT"){
+       dashboardId = '5e248c29-45e3-4968-a330-8f68a109facc';
+   }
+   
+   if(req.params.id == "TREE"){
+       dashboardId = '87d6abd9-1ffc-473a-99ff-0d8d302d29f3';
+   }
+ 
+   quicksight.getDashboardEmbedUrl({
         // Enter your actual AWS account ID
         'AwsAccountId': '998776844619', 
         'DashboardId': dashboardId, 
@@ -64,6 +94,9 @@ app.get('/url/:id', function(req, res){
         res.send({url: data.EmbedUrl});
     });
 });
-app.listen(3000);
+//app.listen(9001, function(){
+//console.log('listening');
+//});
+https.createServer(options, app).listen(9001, () => console.log(`Server started at port : 9001`));
 
 
